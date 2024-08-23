15b61247dd92333cd920f2aab9a15d4668d9e305
mateopolci <mateopolci@hotmail.com>
2024-08-23 18:27:37 -0300

    working transition between main and welcome

--- app/src/main/java/com/example/kotlin_login/WelcomeActivity.kt
+++ app/src/main/java/com/example/kotlin_login/WelcomeActivity.kt
@@ -10,8 +10,8 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
