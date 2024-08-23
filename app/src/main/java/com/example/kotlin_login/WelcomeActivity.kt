37fb8dbc3790ddeef3508e98a84c2e9ab0b561a7
oriana-monaldi <orimonaldi@gmail.com>
2024-08-23 19:39:04 -0300

    working os picker

--- app/src/main/java/com/example/kotlin_login/WelcomeActivity.kt
+++ app/src/main/java/com/example/kotlin_login/WelcomeActivity.kt
@@ -1,20 +1,29 @@
 package com.example.kotlin_login
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.CheckBox
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val checkboxIOS = findViewById<CheckBox>(R.id.logo_ios)
+        val checkboxAndroid = findViewById<CheckBox>(R.id.logo_android)
+        val imageViewIOS = findViewById<ImageView>(R.id.imageview_ios)
+        val imageViewAndroid = findViewById<ImageView>(R.id.imageview_android)
+
+        checkboxIOS.setOnCheckedChangeListener { _, isChecked ->
+            imageViewIOS.visibility = if (isChecked) ImageView.VISIBLE else ImageView.GONE
+        }
+
+        checkboxAndroid.setOnCheckedChangeListener { _, isChecked ->
+            imageViewAndroid.visibility = if (isChecked) ImageView.VISIBLE else ImageView.GONE
         }
     }
+
 }
+
+
